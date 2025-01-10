e60404c073fa6b159bd666eeed908b47bc802f13
Bonattotomas123 <tomasabreu.me@gmail.com>
2025-01-10 10:59:16 +0000

    Correção de alguns bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,31 +92,12 @@
   /**
    * Animation on scroll function and init
    */
-  function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', aosInit);
+  
 
   /**
    * Init typed.js
    */
-  const selectTyped = document.querySelector('.typed');
-  if (selectTyped) {
-    let typed_strings = selectTyped.getAttribute('data-typed-items');
-    typed_strings = typed_strings.split(',');
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  
 
   /**
    * Animate the skills items on reveal

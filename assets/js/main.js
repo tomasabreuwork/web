d9eb9ee7f18f81632ed94736fd211fac04d14815
Bonattotomas123 <tomasabreu.me@gmail.com>
2025-05-30 11:06:53 +0100

    Adição do cursor para o rato

--- assets/js/main.js
+++ assets/js/main.js
@@ -314,3 +314,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+  const cursor = document.getElementById('cursor');
+  document.addEventListener('mousemove', e => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+  });
